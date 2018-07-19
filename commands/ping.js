295894d2fc7f8e2352b446fd06fe70128b2aca44
Faulty X <40147859+faultyx@users.noreply.github.com>
2018-07-19 12:21:35 +0100

    Add files via upload

---
+++ commands/ping.js
@@ -0,0 +1,10 @@
+exports.run = async (bot, message, args) => {
+
+  let msg = await message.channel.send("Pinging");
+  msg.edit(`:ping_pong:Pong! \`${msg.createdTimestamp - message.createdTimestamp}ms\``);
+
+}
+
+exports.help = {
+  name: "ping"
+}
