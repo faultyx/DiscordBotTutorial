d8c4fffcd80a258a99087a2fd0537a17d148bd23
Faulty X <40147859+faultyx@users.noreply.github.com>
2018-07-19 12:20:10 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 // Packages required!
 const Discord = require("discord.js");
+const fs = require("fs");
 
 // config for prefix and login token
 const config = require("./config.json");
 
 // defining our discord client
 const bot = new Discord.Client();
+
+bot.commands = new Discord.Collection();
+
+// command handler
+fs.readdir("./commands/", (err, files) => {
+
+if (err) console.log(err);
+
+let jsfile = files.filter(f => f.split(".").pop() === "js")
+if(jsfile.length <= 0){
+console.log("No files in the commands folder!")
+return;
+}
+
+jsfile.forEach((f, i) => {
+
+let props = require(`./commands/${f}`);
+console.log(`${f} was Loaded!`);
+bot.commands.set(props.help.name, props);
+
+});
+
+});
 
 // Listener Events
 bot.on("ready", async () => {
@@ -28,12 +52,14 @@ if (!message.content.startsWith(prefix)) return;
 
 let msg = message.content.toLowerCase()
 
-let args = message.content.slice(prefix.length).slice(" ");
+let messageArray = message.content.split(" ");
 
-if (msg.startsWith(prefix + "ping")) {
-  let msg = await message.channel.send("Pinging");
-  msg.edit(`:ping_pong:Pong! \`${msg.createdTimestamp - message.createdTimestamp}ms\``);
-}
+let cmd = messageArray[0];
+
+let args = messageArray.slice(1);
+
+let commandFile = bot.commands.get(cmd.slice(prefix.length));
+if (commandFile) commandFile.run(bot, message, args);
 
 });
 
