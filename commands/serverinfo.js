fd9664ba14b3a1d3f9003953e35d3a96bd3b96f4
Faulty X <40147859+faultyx@users.noreply.github.com>
2019-07-02 16:02:15 +0100

    update deprecated code.

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,21 +1,16 @@
-// discord.js pck required
 const Discord = require("discord.js");
-exports.run = async (bot, message, args) => {
-let icon = message.guild.iconURL;
+module.exports.run = (bot, message, args) => {
 const embed = new Discord.RichEmbed()
-.setTitle("Serverinfo")
-.setThumbnail(icon)
+.setTitle(`${message.guild.name} | ${message.guild.id}`)
+.setThumbnail(message.guild.iconURL)
 .setColor("#42f4e2")
 .setTimestamp()
-.addField("Owner:", message.guild.owner)
-.addField("Owner ID:", message.guild.owner.id)
-.addField("Server Name:", message.guild.name)
-.addField("Server Created At:", message.guild.createdAt)
-.addField("Server ID:", message.guild.id)
+.addField("Owner:", `${message.guild.owner.user.tag} | ${message.guild.owner.user.id}`)
+.addField("Created At:", message.guild.createdAt)
 .addField("Member Count:", message.guild.memberCount)
-message.channel.send(embed)
+message.channel.send(embed);
 
-}
-exports.help = {
+};
+module.exports.help = {
   name: "serverinfo"
-}
+};
