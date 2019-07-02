e88d64fad1610a4b701bf92deb9d2b0eb6c6d478
Faulty X <40147859+faultyx@users.noreply.github.com>
2019-07-02 16:04:11 +0100

    update deprecated code.

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -7,7 +7,7 @@ let embed = new Discord.RichEmbed()
 .setColor("#4bf442")
 .setAuthor(`${user.tag} | ${user.id}`, user.displayAvatarURL)
 .setTimestamp()
-.addField("Joined Discord:", user.createdAt)
+.addField("Joined Discord:", user.createdAt.toUTCString())
 .addField("User Status:", user.presence.status)
 .addField("User Game:", `${user.presence.game ? user.presence.game.name: 'Not playing anything!'}`)
 message.channel.send(embed);
