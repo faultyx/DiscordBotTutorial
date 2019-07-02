8d50d7fc014c4ade4dca3f69fe2cda75849df770
Faulty X <40147859+faultyx@users.noreply.github.com>
2019-07-02 16:16:43 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,6 @@
-console.log(`${bot.user.tag} is Online!`);
-
+module.exports.run = (bot) => {
 bot.user.setActivity("a tutorial", {type: "WATCHING"});
 
 bot.user.setStatus("online");
+console.log(`${bot.user.tag} is Online!`);
+};
