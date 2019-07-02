708380f6f675ded3b8d37d9865cf9964f0d18cff
Faulty X <40147859+faultyx@users.noreply.github.com>
2019-07-02 15:59:45 +0100

    update deprecated code.

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,18 +1,14 @@
-// discord.js pck required
 const Discord = require("discord.js");
-exports.run = async (bot, message, args) => {
-let avatar = bot.user.displayAvatarURL;
+module.exports.run = (bot, message, args) => {
 const embed = new Discord.RichEmbed()
 .setColor("#9842f4")
-.setThumbnail(avatar)
+.setTitle(bot.user.tag)
+.setThumbnail(bot.user.displayAvatarURL)
 .setTimestamp()
-.setTitle("Botinfo")
-.addField("Bot Username:", bot.user.username)
-.addField("Bot Discrim:", `#${bot.user.discriminator}`)
-.addField("Bot ID:", bot.user.id)
-.addField("Created At:", bot.user.createdAt)
-message.channel.send(embed)
-}
-exports.help = {
+.setFooter(bot.user.id)
+.addField("Created At:", bot.user.createdAt.toUTCString())
+message.channel.send(embed);
+};
+module.exports.help = {
   name: "botinfo"
-}
+};
