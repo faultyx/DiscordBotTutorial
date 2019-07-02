6d0aa5f6c9dc5d97559bf8080e1e35ff022dc579
Faulty X <40147859+faultyx@users.noreply.github.com>
2019-07-02 16:18:13 +0100

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,1 +1,18 @@
+const config = require("../config.json");
+module.exports.run = async (bot, message) => {
 
+if (message.author.bot) return;
+
+let prefix = config.defaultPrefix;
+
+if (!message.content.startsWith(prefix)) return;
+
+let args = message.content.split(" ").slice(1);
+  
+let command = args.shift();
+  
+let cmd = bot.commands.get(command);
+  
+cmd.run(bot, message, args);
+    
+};
