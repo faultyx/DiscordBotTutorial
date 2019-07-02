05af77140b9734b296e94b06fa0d274d280ba033
Faulty X <40147859+faultyx@users.noreply.github.com>
2019-07-02 16:03:35 +0100

    update deprecated code.

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,20 +1,17 @@
-const Discord = require("discord.js"); // required Package
-exports.run = async (bot, message, args) => {
+const Discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
 
 let user = message.mentions.users.first() || message.author;
 
 let embed = new Discord.RichEmbed()
 .setColor("#4bf442")
-.setAuthor(`${user.tag}'s Info`, `${user.avatarURL}`)
-.setFooter(`User ID: ${user.id}`)
+.setAuthor(`${user.tag} | ${user.id}`, user.displayAvatarURL)
 .setTimestamp()
-.addField("Discord Name:", user.username)
-.addField("Discriminator:", `#${user.discriminator}`)
 .addField("Joined Discord:", user.createdAt)
 .addField("User Status:", user.presence.status)
 .addField("User Game:", `${user.presence.game ? user.presence.game.name: 'Not playing anything!'}`)
-message.channel.send(embed)
-}
-exports.help = {
+message.channel.send(embed);
+};
+module.exports.help = {
   name: "userinfo"
-}
+};
